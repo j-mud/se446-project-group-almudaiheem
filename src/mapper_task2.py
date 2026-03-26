1b705b8750d1b1ad09984ea21ff37267b0902930
lauraalsubaie-arch <lauraalsubaie@gmail.com>
2026-03-26 19:53:03 +0300

    Implement crime type extraction from input

--- src/mapper_task2.py
+++ src/mapper_task2.py
@@ -1,1 +1,14 @@
+import sys
 
+for line in sys.stdin:
+    line = line.strip()
+    fields = line.split(',')
+
+    # Skip header
+    if fields[0] == 'ID':
+        continue
+
+    # Make sure column exists
+    if len(fields) > 5:
+        crime_type = fields[5].strip()
+        print(f"{crime_type}\t1")
