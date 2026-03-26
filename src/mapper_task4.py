9546c86f1f2afad7873870cedd1dd64d15a52463
FjrSad <Fsad@alfaisal.edu>
2026-03-26 21:16:08 +0300

    Task 4: Add mapper_task4.py

--- src/mapper_task4.py
+++ src/mapper_task4.py
@@ -1,1 +1,16 @@
+import sys
 
+for line in sys.stdin:
+    line = line.strip()
+    fields = line.split(',')
+
+    if fields[0] == 'ID':
+        continue
+
+    if len(fields) > 2:
+        date_str = fields[2].strip()
+        try:
+            year = date_str.split('/')[2].split(' ')[0]
+            print(f'{year}\t1')
+        except IndexError:
+            continue
